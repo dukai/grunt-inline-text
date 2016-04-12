374124bd6b9d4ec1a8b48f356e5cb6fc51e866b5
dukai <dukai86@gmail.com>
2016-04-12 15:07:05 +0800

    init project

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+/*
+ * grunt-inline-text
+ * 
+ *
+ * Copyright (c) 2016 dukai
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    config: {
+      dist: 'tmp'
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    inline_text: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!'
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      },
+
+      myself: {
+        options:{},
+        files: [{
+          expand: true,
+          cwd: 'test',
+          src: 'app/**/*.tmpl',
+          dest: 'test'
+        }]
+      }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'inline_text:myself']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/inline_text.js
@@ -0,0 +1,52 @@
+/*
+ * grunt-inline-text
+ * 
+ *
+ * Copyright (c) 2016 dukai
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var fs = require('fs');
+
+var Template = require('template');
+
+
+
+module.exports = function(grunt) {
+
+
+  var tmpl = new Template(grunt.file.read('tasks/fn_template'));
+
+  grunt.registerMultiTask('inline_text', 'Package the requirejs text plugin module content to inline amd javascript module, so after these processing, the modules that have text! dependencies can be used cross domains.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      var src = f.src[0];
+      var dest = f.dest + '.js';
+      grunt.log.debug('dest target', dest);
+
+      var fileContent = grunt.file.read(src);
+      grunt.log.debug('file content', fileContent);
+
+      fileContent = fileContent.replace(/\r/g, '\\r')
+          .replace(/\n/g, '\\n')
+          .replace(/\t/g, '')
+          .replace(/'/g, "\\\'");
+
+
+      // Write the destination file.
+      grunt.file.write(dest, tmpl.render({
+          content: fileContent
+      }));
+
+      // Print a success message.
+      grunt.log.writeln('File "' + dest + '" created.');
+    });
+  });
+
+};
